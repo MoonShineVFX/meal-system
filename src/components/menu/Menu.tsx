3e42c538b79e6dfbc84d3237b76c17f7d6269cc0
eli <mrhchief@gmail.com>
2022-12-10 21:13:41 +0800

    feat: scroll to top when changing category

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,5 +1,5 @@
 import type { MenuType } from '@prisma/client'
-import { useMemo, useState, useCallback, useEffect } from 'react'
+import { useMemo, useState, useCallback, useEffect, useRef } from 'react'
 import { useRouter } from 'next/router'
 
 import { twData, settings } from '@/lib/common'
@@ -25,6 +25,7 @@ export default function Menu(props: { type: MenuType; date?: Date }) {
     CommoditiesOnMenu[0] | null
   >(null)
   const [isDetailOpen, setIsDetailOpen] = useState(false)
+  const detailRef = useRef<HTMLDivElement>(null)
 
   // Detect commodityId from query and open
   useEffect(() => {
@@ -41,6 +42,18 @@ export default function Menu(props: { type: MenuType; date?: Date }) {
       setIsDetailOpen(false)
     }
   }, [router.query.commodityId, data])
+
+  // Detect category from query and scroll to top
+  useEffect(() => {
+    if (router.query.category && data) {
+      setCurrentCategory(router.query.category as string)
+    } else if (!router.query.category) {
+      setCurrentCategory(settings.MENU_CATEGORY_ALL)
+    }
+    if (detailRef.current) {
+      detailRef.current.scrollTop = 0
+    }
+  }, [router.query.category, data, detailRef.current])
 
   // Group commodities by category
   const commoditiesOnMenuByCategory = useMemo(() => {
@@ -98,9 +111,24 @@ export default function Menu(props: { type: MenuType; date?: Date }) {
     }
   }, [router.query])
 
-  const handleCategoryClick = useCallback((mainCategory: string) => {
-    setCurrentCategory(mainCategory)
-  }, [])
+  const handleCategoryClick = useCallback(
+    (category: string) => {
+      if (router.query.category !== category) {
+        router.push(
+          {
+            pathname: router.pathname,
+            query: {
+              ...router.query,
+              category,
+            },
+          },
+          undefined,
+          { shallow: true },
+        )
+      }
+    },
+    [router],
+  )
 
   if (isError) return <div className='text-red-400'>{error.message}</div>
   if (isLoading) return <Spinner className='h-6 w-6' />
@@ -119,7 +147,7 @@ export default function Menu(props: { type: MenuType; date?: Date }) {
                 selected: currentCategory === mainCategory,
               })}
               key={mainCategory}
-              className='w-fit shrink-0 cursor-pointer rounded-2xl px-3 py-2 text-stone-500 data-selected:pointer-events-none data-selected:bg-yellow-500 data-selected:text-yellow-900 data-not-selected:hover:bg-stone-200'
+              className='w-fit shrink-0 cursor-pointer rounded-2xl px-3 py-2 text-stone-500 data-selected:pointer-events-none data-selected:bg-yellow-500 data-selected:text-yellow-900 data-not-selected:hover:bg-stone-600/10 data-not-selected:active:bg-stone-600/10 lg:data-not-selected:hover:bg-stone-100 lg:data-not-selected:active:bg-stone-100'
               onClick={() => handleCategoryClick(mainCategory)}
             >
               <p className='text-justify font-bold tracking-widest'>
@@ -130,7 +158,10 @@ export default function Menu(props: { type: MenuType; date?: Date }) {
         </ul>
         {/* Commodities */}
         <section className='relative grow'>
-          <div className='absolute inset-0 overflow-y-auto p-4 pt-[88px] lg:p-8'>
+          <div
+            ref={detailRef}
+            className='absolute inset-0 overflow-y-auto p-4 pt-[88px] lg:p-8'
+          >
             <CommodityOnMenuList
               currentCategory={currentCategory}
               commoditiesOnMenuByCategory={commoditiesOnMenuByCategory}
