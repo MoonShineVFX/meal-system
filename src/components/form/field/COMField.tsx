d40918347ab91820aa4afbf6235977084d21b4e2
eli.hung <mrhchief@gmail.com>
2023-02-13 14:37:58 +0800

    feat(COMField): add sort method

--- src/components/form/field/COMField.tsx
+++ src/components/form/field/COMField.tsx
@@ -73,88 +73,111 @@ export default function COMField<T extends FieldValues>(
     <div className={props.formInput.className}>
       <BaseLabel label={props.formInput.label}>
         <div className='flex flex-col gap-2'>
-          {comDatas.map((comData) => {
-            const menu = data.find((menu) => menu.id === comData.menuId)
-            if (!menu) return null
-            return (
-              <div key={comData.menuId} className='rounded-2xl border p-3'>
-                <div className='flex items-center justify-between'>
-                  <h1>{getMenuName(menu)}</h1>
-                  <button
-                    type='button'
-                    className='rounded-full p-1 text-stone-400 hover:bg-stone-200 active:scale-90'
-                    onClick={() =>
-                      setComDatas((prevIds) =>
-                        prevIds.filter(
-                          (prevId) => prevId.menuId !== comData.menuId,
-                        ),
-                      )
-                    }
-                  >
-                    <XMarkIcon className='h-5 w-5' />
-                  </button>
-                </div>
+          {comDatas
+            .map((comData) => {
+              const menu = data.find((menu) => menu.id === comData.menuId)
+              if (!menu) return null
+              return {
+                ...comData,
+                date: menu.date,
+                name: getMenuName(menu),
+              }
+            })
+            .filter((comData) => comData !== null)
+            .sort((a, b) => {
+              if (!a || !b) return 0
+              if (!a.date && !b.date) return b.menuId - a.menuId
+              if (!a.date && b.date) return -1
+              if (a.date && !b.date) return 1
+              if (a.date && b.date) {
+                if (b.date.getTime() - a.date.getTime() === 0) {
+                  return a.menuId - b.menuId
+                }
+                return b.date.getTime() - a.date.getTime()
+              }
+              return b.menuId - a.menuId
+            })
+            .map((comDataOpt) => {
+              const comData = comDataOpt!
+              return (
+                <div key={comData.menuId} className='rounded-2xl border p-3'>
+                  <div className='flex items-center justify-between'>
+                    <h1>{comData.name}</h1>
+                    <button
+                      type='button'
+                      className='rounded-full p-1 text-stone-400 hover:bg-stone-200 active:scale-90'
+                      onClick={() =>
+                        setComDatas((prevIds) =>
+                          prevIds.filter(
+                            (prevId) => prevId.menuId !== comData.menuId,
+                          ),
+                        )
+                      }
+                    >
+                      <XMarkIcon className='h-5 w-5' />
+                    </button>
+                  </div>
 
-                <div className='flex flex-col gap-2 py-2'>
-                  <div className='flex items-center justify-between gap-2'>
-                    <label className='whitespace-nowrap text-sm text-stone-400'>
-                      總數
-                    </label>
-                    <input
-                      type='number'
-                      className='w-1/2 rounded-md border border-stone-300 bg-stone-50 py-1 px-2 text-right focus:border-yellow-500 focus:ring-yellow-500'
-                      defaultValue={0}
-                      value={comData.stock}
-                      min={0}
-                      max={999}
-                      onChange={(e) => {
-                        const value = parseInt(e.target.value)
-                        setComDatas((prevDatas) =>
-                          prevDatas.map((prevData) => {
-                            if (prevData.menuId === comData.menuId) {
-                              return {
-                                ...prevData,
-                                stock: value,
-                                limitPerUser:
-                                  value !== 0
-                                    ? Math.min(value, prevData.limitPerUser)
-                                    : prevData.limitPerUser,
+                  <div className='flex flex-col gap-2 py-2'>
+                    <div className='flex items-center justify-between gap-2'>
+                      <label className='whitespace-nowrap text-sm text-stone-400'>
+                        總數
+                      </label>
+                      <input
+                        type='number'
+                        className='w-1/2 rounded-md border border-stone-300 bg-stone-50 py-1 px-2 text-right focus:border-yellow-500 focus:ring-yellow-500'
+                        defaultValue={0}
+                        value={comData.stock}
+                        min={0}
+                        max={999}
+                        onChange={(e) => {
+                          const value = parseInt(e.target.value)
+                          setComDatas((prevDatas) =>
+                            prevDatas.map((prevData) => {
+                              if (prevData.menuId === comData.menuId) {
+                                return {
+                                  ...prevData,
+                                  stock: value,
+                                  limitPerUser:
+                                    value !== 0
+                                      ? Math.min(value, prevData.limitPerUser)
+                                      : prevData.limitPerUser,
+                                }
                               }
-                            }
-                            return prevData
-                          }),
-                        )
-                      }}
-                    />
-                  </div>
-                  <div className='flex items-center  justify-between gap-2'>
-                    <label className='whitespace-nowrap text-sm text-stone-400'>
-                      每人限購
-                    </label>
-                    <input
-                      type='number'
-                      className='w-1/2 rounded-md border border-stone-300 bg-stone-50 py-1 px-2 text-right focus:border-yellow-500 focus:ring-yellow-500'
-                      defaultValue={0}
-                      min={0}
-                      max={comData.stock === 0 ? 999 : comData.stock}
-                      value={comData.limitPerUser}
-                      onChange={(e) => {
-                        const value = parseInt(e.target.value)
-                        setComDatas((prevDatas) =>
-                          prevDatas.map((prevData) => {
-                            if (prevData.menuId === comData.menuId) {
-                              return { ...prevData, limitPerUser: value }
-                            }
-                            return prevData
-                          }),
-                        )
-                      }}
-                    />
+                              return prevData
+                            }),
+                          )
+                        }}
+                      />
+                    </div>
+                    <div className='flex items-center  justify-between gap-2'>
+                      <label className='whitespace-nowrap text-sm text-stone-400'>
+                        每人限購
+                      </label>
+                      <input
+                        type='number'
+                        className='w-1/2 rounded-md border border-stone-300 bg-stone-50 py-1 px-2 text-right focus:border-yellow-500 focus:ring-yellow-500'
+                        defaultValue={0}
+                        min={0}
+                        max={comData.stock === 0 ? 999 : comData.stock}
+                        value={comData.limitPerUser}
+                        onChange={(e) => {
+                          const value = parseInt(e.target.value)
+                          setComDatas((prevDatas) =>
+                            prevDatas.map((prevData) => {
+                              if (prevData.menuId === comData.menuId) {
+                                return { ...prevData, limitPerUser: value }
+                              }
+                              return prevData
+                            }),
+                          )
+                        }}
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
-            )
-          })}
+              )
+            })}
         </div>
         {/* Dropdown */}
         <DropdownMenu
