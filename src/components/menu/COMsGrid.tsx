dacd3c730450bdcf2b328b17350cc6e0bf6e7eee
eli.hung <mrhchief@gmail.com>
2023-03-08 21:09:51 +0800

    feat(COMsGrid): merge categories coms for reservation display

--- src/components/menu/COMsGrid.tsx
+++ src/components/menu/COMsGrid.tsx
@@ -40,6 +40,26 @@ function COMsGrid(props: {
       : comsByCategoryPlaceHolder)
   const fillerCount = Math.max(TOTAL_FILLER_COUNT - comsByCategory.size)
 
+  // Create new record, key is com id, value is subCategories per com
+  const subCategoriesFromComs = new Map<string, string[]>()
+  if (props.fromReserve) {
+    comsByCategory.forEach((mainCategoryData) => {
+      mainCategoryData.subCategories.forEach((subCategoryData, subCategory) => {
+        subCategoryData.coms.forEach((com) => {
+          if (com) {
+            const comId = com.commodity.id.toString()
+            if (subCategoriesFromComs.has(comId)) {
+              subCategoriesFromComs.get(comId)?.push(subCategory)
+            } else {
+              subCategoriesFromComs.set(comId, [subCategory])
+            }
+          }
+        })
+      })
+    })
+  }
+  const existComIds: number[] = []
+
   return (
     <section className='grid w-full grid-cols-[repeat(auto-fit,minmax(8.5rem,1fr))] gap-4 lg:gap-8'>
       {[...comsByCategory].map(([mainCategory, mainCategoryData], index) => (
@@ -62,13 +82,29 @@ function COMsGrid(props: {
           {[...mainCategoryData.subCategories].map(
             ([subCategory, subCategoryData]) => (
               <Fragment key={subCategory}>
-                {subCategoryData.coms.map((com, index) => (
-                  <COMCard
-                    key={com?.commodity.id ?? index}
-                    com={com}
-                    category={subCategory}
-                  />
-                ))}
+                {subCategoryData.coms.map((com, index) => {
+                  if (
+                    props.fromReserve &&
+                    com &&
+                    existComIds.includes(com?.commodity.id)
+                  ) {
+                    return null
+                  }
+                  if (com) existComIds.push(com?.commodity.id)
+                  return (
+                    <COMCard
+                      key={com?.commodity.id ?? index}
+                      com={com}
+                      category={
+                        props.fromReserve && com
+                          ? subCategoriesFromComs
+                              .get(com.commodity.id.toString())!
+                              .join(' / ')
+                          : subCategory
+                      }
+                    />
+                  )
+                })}
               </Fragment>
             ),
           )}
