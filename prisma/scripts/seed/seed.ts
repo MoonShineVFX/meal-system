87a80f8752b2f96f91d1013b244dd901f59ee38d
eli <mrhchief@gmail.com>
2023-09-12 11:02:55 +0800

    feat: add new seed for pos stress test

--- prisma/scripts/seed/seed.ts
+++ prisma/scripts/seed/seed.ts
@@ -1,10 +1,12 @@
 import seedUser from './user'
 import { seedMenu, seedReservationsMenu } from './menu'
+import { seedLiveOrders } from './order'
 
 async function main() {
-  await seedUser()
-  await seedMenu()
-  await seedReservationsMenu()
+  // await seedUser()
+  // await seedMenu()
+  // await seedReservationsMenu()
+  await seedLiveOrders()
 }
 
 main()
