9f5ca2fa4a0ad694510f6f7e4b0a95b3489c409a
eli <mrhchief@gmail.com>
2023-12-12 16:57:20 +0800

    fix: convert project to esm

--- src/lib/trpc/websocket.ts
+++ src/lib/trpc/websocket.ts
@@ -1,12 +1,12 @@
 import { applyWSSHandler } from '@trpc/server/adapters/ws'
-import ws from 'ws'
+import ws, { WebSocketServer } from 'ws'
 
 import { createContext } from './trpc'
 import { appRouter } from './api/router'
 import { settings } from '@/lib/common'
 
 export function createWebSocketServer(server?: ws.ServerOptions['server']) {
-  const wss = new ws.Server({
+  const wss = new WebSocketServer({
     server: server,
     port: server ? undefined : parseInt(settings.WEBSOCKET_DEV_PORT, 10),
   })
