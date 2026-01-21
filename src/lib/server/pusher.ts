8988cb573537846fe6d64f8fc806fe42f5f68e78
eli.hung <mrhchief@gmail.com>
2026-01-21 09:11:58 +0800

    feat: Separate webhook channel

--- src/lib/server/pusher.ts
+++ src/lib/server/pusher.ts
@@ -4,6 +4,7 @@ import {
   PUSHER_WS_HOST,
   PusherChannel,
   PusherEventPayload,
+  PusherWebhookEvent,
 } from '@/lib/common/pusher'
 import Pusher from 'pusher'
 import { getDebugLogger } from './logger'
@@ -12,6 +13,7 @@ const log = getDebugLogger('pusher')
 
 // Pusher configuration
 let pusherInstance: Pusher | null = null
+const WEBHOOK_CHANNEL = 'webhook' as const
 
 export const getPusherServer = () => {
   if (!pusherInstance) {
@@ -40,6 +42,21 @@ export const emitPusherEvent = async (
     return true
   } catch (error) {
     log(`Error triggering Pusher event: ${error}`)
+    return false
+  }
+}
+
+export const emitPusherWebhook = async (
+  event: PusherWebhookEvent,
+  message: any,
+) => {
+  try {
+    const pusher = getPusherServer()
+    await pusher.trigger(WEBHOOK_CHANNEL, event, message)
+    log(`Triggered webhook with event ${event}`)
+    return true
+  } catch (error) {
+    log(`Error triggering Pusher webhook: ${error}`)
     return false
   }
 }
